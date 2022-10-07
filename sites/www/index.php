85f442106e69548358a49a4c83675e6ae510bd43
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-10-07 10:55:15 +0200

    Abrir en enlace en una nueva pestaña

--- sites/www/index.php
+++ sites/www/index.php
@@ -9,7 +9,7 @@
 <body>
 <h1>dockerbox</h1>
 <p>Entorno de desarrollo para programación web con PHP en Docker.</p>
-<p>Más información en el <a href="https://github.com/ijaureguialzo/dockerbox">repositorio de GitHub</a>.</p>
+<p>Más información en el <a target="_blank" href="https://github.com/ijaureguialzo/dockerbox">repositorio de GitHub</a>.</p>
 <hr>
 <p>
     <?php
